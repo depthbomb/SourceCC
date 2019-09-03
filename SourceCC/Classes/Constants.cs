bdc2ebd96e509ba060fb70fbe4cdc4735c43b027
depthbomb <reesepup77@hotmail.com>
2019-09-03 01:36:07 -0500

    Remove unused resources

--- SourceCC/Classes/Constants.cs
+++ SourceCC/Classes/Constants.cs
@@ -4,9 +4,9 @@ namespace SourceCC.Classes
 {
     class Constants
     {
-        public static readonly string Language       = Properties.Settings.Default.Language;
-        public static readonly bool Is64BitOs        = Environment.Is64BitOperatingSystem;
-        public static readonly string TF2DefaultPath = Is64BitOs ? @"C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf" : @"C:\Program Files\Steam\SteamApps\common\Team Fortress 2\tf";
+        public static readonly string Language        = Properties.Settings.Default.Language;
+        public static readonly bool Is64BitOs         = Environment.Is64BitOperatingSystem;
+        public static readonly string TF2DefaultPath  = Is64BitOs ? @"C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf" : @"C:\Program Files\Steam\SteamApps\common\Team Fortress 2\tf";
         public static readonly string L4D2DefaultPath = Is64BitOs ? @"C:\Program Files (x86)\Steam\SteamApps\common\Left 4 Dead 2\left4dead2" : @"C:\Program Files\Steam\SteamApps\common\Left 4 Dead 2\left4dead2";
     }
 }
